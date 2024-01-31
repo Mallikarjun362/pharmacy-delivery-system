d9b1305e24cbb63a0c7dad933912cf2299260ae2
hemanthgaddey <hemanthgaddey@gmail.com>
2024-01-31 15:47:06 +0530

    Payment via upi complete

--- src/app/buyer/cart/page.tsx
+++ src/app/buyer/cart/page.tsx
@@ -21,7 +21,28 @@ export default function CartPage() {
   const { cart } = useGlobalContext();
   const [is_prescription_required, setIsPrescriptionRequired] = useState(false);
   const [file, setFile] = useState<any>(null);
-  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [totalPrice, setTotalPrice] = useState<number>(0);  
+  const [paymentMethod, setPaymentMethod] = useState('cod'); // Default to 'Cash On Delivery';
+  const [selectedOption, setSelectedOption] = useState<string>('');
+
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
+  };
+
+  const RadioButtonStyles = `
+  .paymentMethodContainer {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+  }
+
+  .paymentMethodLabel {
+    margin-bottom: 5px;
+  }
+
+  .paymentMethodRadio {
+    margin-right: 5px;
+  }`
 
   useEffect(() => {
     let s = 1;
@@ -35,21 +56,6 @@ export default function CartPage() {
   }, []);
 
   const handlePayNow = async () => {
-    if (is_prescription_required && !file)
-      return alert('Prescription required');
-    if (Object.keys(cart)?.length === 0) {
-      return alert('Cart is empty');
-    }
-    const formData = new FormData();
-    formData.append('file', file);
-    await placeOrderManual({
-      cart,
-      buyer_db_id: session?.data?.user?.custome_data.db_id,
-      seller_db_id: (Object.values(cart)[0] as any).seller_db_id,
-      prescription_file: file ? formData : null,
-    });
-    alert('Order confirmed successfully');
-
     window.location.replace(`upi://pay?pa=9494248739@paytm&amp;pn=Mr Gaddey Hemanth Chowdary&amp;am=${totalPrice}&amp;mc=bdOMam96965720364044&amp;cu=INR;tr=${generateRandomString()};tn=pharmacy delivery payment`);
   };  
 
@@ -98,29 +104,32 @@ export default function CartPage() {
         <div>Prescription not required</div>
       )}
       <div>Total price : &#8377; {totalPrice}</div>
-      <button
-        onClick={async () => {
-          window.location.replace(`upi://pay?pa=9494248739@paytm&amp;pn=Mr Gaddey Hemanth Chowdary&amp;am=10&amp;mc=bdOMam96965720364044&amp;cu=INR;tr=20sssj9j3s8j38;tn=business_related_stuff`);
 
-          // if (is_prescription_required && !file)
-          //   return alert('Prescription required');
-          // if (Object.keys(cart)?.length === 0) {
-          //   return alert('Cart is empty');
-          // }
-          // const formData = new FormData();
-          // formData.append('file', file);
-          // await placeOrderManual({
-          //   cart,
-          //   buyer_db_id: session?.data?.user?.custome_data.db_id,
-          //   seller_db_id: (Object.values(cart)[0] as any).seller_db_id,
-          //   prescription_file: file ? formData : null,
-          // });
-          // alert('Order confirmed successfully');
+      <p>Select Payment Method:</p>
+      <div style={{ display: 'flex', flexDirection: 'column', marginLeft:'20px', marginTop: '10px' }}>
+        <label>
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="cod"
+            checked={paymentMethod === 'cod'}
+            onChange={() => setPaymentMethod('cod')}
+          />
+          Cash On Delivery
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="upi"
+            checked={paymentMethod === 'upi'}
+            onChange={() => setPaymentMethod('upi')}
+          />
+          UPI
+        </label>
+      </div>
 
-        }}
-      >
-      </button>
-
+      {(paymentMethod=='upi') &&
       <button
         onClick={
           handlePayNow
@@ -128,18 +137,51 @@ export default function CartPage() {
           //window.location.href = 'upi://pay?pa=9494248739@paytm&amp;pn=Mr Gaddey Hemanth Chowdary&amp;am=10&amp;mc=bdOMam96965720364044&amp;cu=INR;tr=20sssj9j3s8j38;tn=business_related_stuff'; // Replace with your external link
         }
         style={{
-          backgroundColor: '#2196F3',  // Replace with your desired background color
+          backgroundColor: '#0aab5b',//'#2196F3',  // Replace with your desired background color
           color: 'white',
           padding: '10px 15px',
           border: 'none',
-          margin: '20px',
+          marginLeft: '0px',
+          marginTop: '20px',
           borderRadius: '5px',
           cursor: 'pointer',
-          marginTop: '10px',  // Add margin for spacing
         }}
       >
-        PAY MONEY
+        Pay Money
+      </button>}
+
+      {(paymentMethod=='cod') && <button
+        style={{
+          backgroundColor: '#eba434',  // Replace with your desired background color
+          color: 'white',
+          padding: '10px 15px',
+          border: 'none',
+          marginLeft: '0px',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          marginTop: '20px',  // Add margin for spacing
+        }}
+        onClick={async () => {
+          if (is_prescription_required && !file)
+            return alert('Prescription required');
+          if (Object.keys(cart)?.length === 0) {
+            return alert('Cart is empty');
+          }
+          const formData = new FormData();
+          formData.append('file', file);
+          await placeOrderManual({
+            cart,
+            buyer_db_id: session?.data?.user?.custome_data.db_id,
+            seller_db_id: (Object.values(cart)[0] as any).seller_db_id,
+            prescription_file: file ? formData : null,
+          });
+          alert('Order confirmed successfully');
+
+        }}
+      >
+        Place Order
       </button>
+      }
 
       {/* <button onClick={handlePayNow} className="payNowButton">
         PAY MONEY
