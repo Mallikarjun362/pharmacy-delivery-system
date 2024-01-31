e836678597487e15a612724dc9d283fefb210860
chaitanyasai <anumulacs@iitbhilai.ac.in>
2024-01-31 12:15:03 +0530

    made ui for browser page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,10 +1,14 @@
 import SearchBar from "@/app/_components/BrowserSearchBar";
+import Card from "@/app/_components/ProductCard";
 import "@/app/globals.css";
 
 export default function BrowsePage() {
     return <main>
         <div className="search">
             <SearchBar />
+        </div>
+        <div className="card_display">
+            <Card />
         </div>
     </main>;
 }
